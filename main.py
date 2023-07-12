577ebd91f8cc6ca7b25ec0a2db3fbac26453d8d8
Koba <110396178+NamoradoDoPedro@users.noreply.github.com>
2023-07-12 10:29:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import pygame as pg
+from pygame import Vector2 as v
 from pygame.locals import *
 from random import randint as r
 from sys import exit
 
 pg.init()
-v = pg.Vector2
 
 
 class Display:
-    def __init__(self):
-        self.dimension = v(400, 450)
-        self.display = pg.display.set_mode((self.dimension))
-        self.title = pg.display.set_caption("Flappy Bird")
+    dimension = v(400, 450)
+    display = pg.display.set_mode((dimension))
+    title = pg.display.set_caption("Flappy Bird")
 
 
 class Bird:
@@ -23,20 +22,22 @@ class Bird:
 
         self.color = (255, 255, 0)
         self.score = 0
-        self.lockMoves = False
+        self.lock = False
+        self.died = False
 
     def move(self):
-        self.acceleration = v(0, 0.5)
-        self.velocity += self.acceleration
-        self.position += self.velocity + 0.5 * self.acceleration
+        if not self.lock:
+            self.acceleration = v(0, 0.5)
+            self.velocity += self.acceleration
+            self.position += self.velocity + 0.5 * self.acceleration
 
-        if self.position.x > DISPLAY.dimension.x:
-            self.position.x = 0
-        if self.position.x < 0:
-            self.position.x = DISPLAY.dimension.x
+            if self.position.x > Display.dimension.x:
+                self.position.x = 0
+            if self.position.x < 0:
+                self.position.x = Display.dimension.x
 
     def jump(self):
-        if self.position.y > 55:
+        if self.position.y > 10:
             self.velocity.y = -8.5
 
     def reset(self):
@@ -47,95 +48,118 @@ class Bird:
 
         self.color = (255, 255, 0)
         self.score = 0
-        self.lockMoves = False
+        self.lock = False
+        self.died = False
 
 
 class Floor:
-    def __init__(self):
-        self.dimension = v(DISPLAY.dimension.x, 20)
-        self.position = v(0, DISPLAY.dimension.y - self.dimension.y)
-        self.color = (255, 0, 0)
+    dimension = v(Display.dimension.x, 20)
+    position = v(0, Display.dimension.y - dimension.y)
+    color = (255, 0, 0)
 
 
 class Pipe:
     def __init__(self):
-        self.dimension = v(50, DISPLAY.dimension.y)
+        self.dimension = v(50, Display.dimension.y)
         self.color = (30, 230, 30)
         self.pipe_gap = 120
-        self.down_pos = v(DISPLAY.dimension.x, r(
-            self.pipe_gap + floor.dimension.y, DISPLAY.dimension.y))
-        self.top_pos = v(DISPLAY.dimension.x,
-                         self.down_pos.y - DISPLAY.dimension.y - self.pipe_gap)
+        self.down_pos = v(Display.dimension.x,
+                          r(self.pipe_gap, Display.dimension.y - self.pipe_gap + Floor.dimension.y))
+        self.top_pos = v(Display.dimension.x,
+                         self.down_pos.y - Display.dimension.y - self.pipe_gap)
+        self.velocity = v(5, 0)
+        self.not_reset = False
+
+    def reset(self):
+        self.down_pos = v(Display.dimension.x,
+                          r(self.pipe_gap + Floor.dimension.y, Display.dimension.y))
+        self.top_pos = v(Display.dimension.x,
+                         self.down_pos.y - Display.dimension.y - self.pipe_gap)
         self.velocity = v(5, 0)
         self.not_reset = False
 
     def move(self):
         self.top_pos = self.top_pos - self.velocity
         self.down_pos = self.down_pos - self.velocity
-        if self.top_pos.x < bird.position.x and not self.not_reset:
-            bird.score += 1
-            self.not_reset = True
-        if self.top_pos.x < -50:
-            self.reset()
-
-    def reset(self):
-        self.down_pos = v(DISPLAY.dimension.x, r(
-            self.pipe_gap + floor.dimension.y, DISPLAY.dimension.y))
-        self.top_pos = v(DISPLAY.dimension.x,
-                         self.down_pos.y - DISPLAY.dimension.y - self.pipe_gap)
-        self.velocity = v(5, 0)
-        self.not_reset = False
 
 
-def draw():
-    re = pg.draw.rect
-    pt = re(DISPLAY.display, pipe.color, (pipe.top_pos, pipe.dimension))
-    pd = re(DISPLAY.display, pipe.color, (pipe.down_pos, pipe.dimension))
-    f = re(DISPLAY.display, floor.color, (floor.position, floor.dimension))
-    p = re(DISPLAY.display, bird.color, (bird.position, bird.dimension))
+class Game:
+    def __init__(self):
+        self._b = Bird()
+        self._p = Pipe()
 
-    if p.colliderect(f) or p.colliderect(pt) or p.colliderect(pd):
-        bird.lockMoves = True
-        pipe.velocity = v(0, 0)
+    def draw(self):
+        ds = Display.display
+        ds.fill((0, 0, 0))
+        pt = pg.draw.rect(ds, self._p.color,
+                          (self._p.top_pos, self._p.dimension))
+        pd = pg.draw.rect(ds, self._p.color,
+                          (self._p.down_pos, self._p.dimension))
+        f = pg.draw.rect(ds, Floor.color,
+                         (Floor.position, Floor.dimension))
+        b = pg.draw.rect(ds, self._b.color,
+                         (self._b.position, self._b.dimension))
 
+        if b.colliderect(f) or b.colliderect(pt) or b.colliderect(pd):
+            self._b.died = True
+            self._p.velocity = v(0, 0)
+            if b.colliderect(f):
+                self._b.lock = True
 
-def showScore():
-    FONT = pg.font.get_default_font()
-    SIZE = 110
-    SysFont = pg.font.SysFont(FONT, SIZE).render
-    blit = DISPLAY.display.blit
-    WHITE = (255, 255, 255)
+    def show_score(self):
+        FONT = pg.font.get_default_font()
+        SIZE = 110
+        WHITE = (255, 255, 255)
+        blit = Display.display.blit
 
-    Text_Player_Points = SysFont(str(bird.score), True, WHITE)
+        Text_Player_Points = pg.font.SysFont(FONT, SIZE).render(
+            str(self._b.score), True, WHITE)
 
-    if bird.score < 10:
-        blit(Text_Player_Points,
-             ((DISPLAY.dimension.x/2) - SIZE/5, 30))
+        if self._b.score < 10:
+            blit(Text_Player_Points,
+                 ((Display.dimension.x/2) - SIZE/5, 30))
 
-    elif bird.score > 9:
-        blit(Text_Player_Points,
-             ((DISPLAY.dimension.x/2) - SIZE/2.5, 30))
+        elif self._b.score > 9:
+            blit(Text_Player_Points,
+                 ((Display.dimension.x/2) - SIZE/2.5, 30))
 
-    else:
-        blit(Text_Player_Points,
-             ((DISPLAY.dimension.x/2) - SIZE, 30))
+        else:
+            blit(Text_Player_Points,
+                 ((Display.dimension.x/2) - SIZE, 30))
 
-    if bird.lockMoves == True:
-        Text_Game_Over = pg.font.SysFont(FONT, 100).render(
-            "Game Over", True, WHITE)
-        Text_Restart = pg.font.SysFont(FONT, 20).render(
-            "Press 'Space' to restart", True, WHITE)
+        if self._b.died == True:
+            blit(pg.font.SysFont(FONT, 100).render(
+                "Game Over", True, WHITE), (10, 140))
+            blit(pg.font.SysFont(FONT, 20).render(
+                "Press 'Space' to restart", True, WHITE), (120, 210))
 
-        blit(Text_Game_Over, (10, 140))
-        blit(Text_Restart, (120, 210))
+    def move(self):
+        key = pg.key.get_pressed()
+        if key[K_SPACE] and self._b.died == False:
+            self._b.jump()
+
+        if self._b.died == True:
+            if key[K_SPACE]:
+                for entity in [self._b, self._p]:
+                    entity.reset()
+
+    def update(self):
+        self.draw()
+        self.show_score()
+        for entity in [self._b, self._p]:
+            entity.move()
+
+        if self._p.top_pos.x < self._b.position.x and not self._p.not_reset:
+            self._b.score += 1
+            self._p.not_reset = True
+
+        if self._p.top_pos.x < -50:
+            self._p.reset()
 
 
 if __name__ == "__main__":
     FPS = 60
-    DISPLAY = Display()
-    bird = Bird()
-    floor = Floor()
-    pipe = Pipe()
+    game = Game()
     clock = pg.time.Clock()
 
     while True:
@@ -145,20 +169,8 @@ if __name__ == "__main__":
                 exit()
 
             if event.type == pg.KEYDOWN:
-                key = pg.key.get_pressed()
-                if key[K_SPACE] and bird.lockMoves == False:
-                    bird.jump()
+                game.move()
 
-                if bird.lockMoves == True:
-                    if key[K_SPACE]:
-                        for entity in [bird, pipe]:
-                            entity.reset()
-
-        for entity in [bird, pipe]:
-            entity.move()
-
-        DISPLAY.display.fill((0, 0, 0))
-        draw()
-        showScore()
+        game.update()
         clock.tick(FPS)
         pg.display.update()
